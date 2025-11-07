0112800d65a5c3d34d8992c9bbd245c36f5ee132
Marcos V. Sousa <marcos.mark1423@gmail.com>
2025-11-07 01:16:11 -0300

    Delete apps directory

--- apps/autenticacao/views.py
+++
@@ -1,7 +0,0 @@
-﻿from django.shortcuts import render
-
-def login_view(request):
-    return render(request, 'autenticacao/login.html')
-
-def register_view(request):
-    return render(request, 'autenticacao/register.html')
--- apps/vendas/views.py
+++
@@ -1,4 +0,0 @@
-﻿from django.shortcuts import render
-
-def index(request):
-    return render(request, 'vendas/index.html')
